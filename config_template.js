150ce631a3950aafcc5401f0225c1d9815fea20c
matt <matt.a.denney@gmail.com>
2025-05-27 14:47:01 -0700

    update

---
+++ config_template.js
@@ -0,0 +1,7 @@
+// replace path names 
+// change file name -> config.js
+const outputDir = '/path/to/output/dir';
+const ssbmIsoPath = '/path/to/ssbm/iso';
+const dolphinPath = '/path/to/dolphin/executable';
+
+export { outputDir, ssbmIsoPath, dolphinPath };
